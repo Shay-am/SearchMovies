d9540626ea836df174d690bdc176aad393b74aab
Mateusz Bogacz <shay_am@wp.pl>
2022-01-07 08:52:50 +0100

    created header in MoviesPaage

--- src/Pages/MoviesPage/MoviesPage.jsx
+++ src/Pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
+import { H1 } from 'Components/Atoms';
+import { Wrapper, Header } from './MoviesPage.styled';
 export const MoviesPage = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Header>
+        <H1>Movies List</H1>
+      </Header>
+    </Wrapper>
+  );
 };
---
+++ src/Pages/MoviesPage/MoviesPage.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  width: 100%;
+  height: 20rem;
+  align-items: flex-end;
+  justify-content: center;
+`;
